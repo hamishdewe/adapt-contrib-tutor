26bfcd1c5c46cca3ed3ea2ba6c2c2cdc07fcb43f
Hamish Dewe <hamish.dewe@gmail.com>
2021-04-15 07:57:05 +1200

    Allow display of feedback as modal (default) or inline left/right of the question

--- js/adapt-contrib-tutor.js
+++ js/adapt-contrib-tutor.js
@@ -4,13 +4,13 @@ define([
 
   Adapt.on('questionView:showFeedback', function(view) {
 
-    var alertObject = {
-      title: view.model.get('feedbackTitle'),
-      body: view.model.get('feedbackMessage')
-    };
-
+    var alertObject;
     var attributes = {};
     var classes = [];
+    var config = Adapt.config.get('_tutor');
+    config = config ? config : {
+      "displayFeedback": "modal"
+    }
 
     if (view.model.has('_isCorrect')) {
       // Attach specific classes so that feedback can be styled.
@@ -36,20 +36,77 @@ define([
     } else if (view.model.has('_extension')) {
       classes.push('is-extension is-' + view.model.get('_extension').toLowerCase());
     }
+    
+    switch (config.displayFeedback) {
+      case 'left':
+      case 'right': {
+        var qId = view.model.get('_id');
+        var qElement = $(`div[data-adapt-id=${qId}]`)[0];
+        var fComponent = $(`div[data-adapt-id=${qId}-feedback]`)[0] || document.createElement('div');
+        var qAlign = config.displayFeedback === 'left' ? 'is-right' : 'is-left';
+        var fAlign = config.displayFeedback === 'left' ? 'is-left' : 'is-right';
+        
+        $(qElement).removeClass(['is-full','is-left-','is-right']).addClass(qAlign);
+        
+        fComponent.classList.add('component');
+        fComponent.classList.add('text');
+        fComponent.classList.add(`${qId}-feedback`);
+        fComponent.classList.add(`${fAlign}`);
+        $(fComponent).attr('data-adapt-id', `${qId}-feedback`);
+        fComponent.innerHTML = `
+          <div class="component__inner text__inner">
+            <div class="component__header text__header">
+              <div class="component__header-inner text__header-inner">
+                <div class="component__title text__title">
+                  <div class="js-heading">
+                    <div id="${qId}-feedback-heading" class="js-heading-inner" role="heading" aria-level="4">
+                      <span class="aria-label">
+                          ${view.model.get('feedbackTitle')}
+                      </span>
+                    </div>
+                  </div>
+                  <h4 class="component__title-inner text__title-inner" aria-hidden="true">
+                    ${view.model.get('feedbackTitle')}
+                  </h4>
+                </div>
+                <div class="component__body text__body">
+                  <div class="component__body-inner text__body-inner">
+                    ${view.model.get('feedbackMessage')}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>`;
+        if (fAlign === 'is-right') {
+          $(qElement).after(fComponent);
+        } else if (fAlign === 'is-left') {
+          $(qElement).before(fComponent);
+        }
+        break;
+      }
+      case 'modal': 
+      default: {
+        alertObject = {
+          title: view.model.get('feedbackTitle'),
+          body: view.model.get('feedbackMessage')
+        };
 
-    // Add the _id property as attribute.
-    attributes['data-adapt-id'] = view.model.get('_id');
+        // Add the _id property as attribute.
+        attributes['data-adapt-id'] = view.model.get('_id');
+        
+        alertObject._classes = classes.join(' ');
+        alertObject._attributes = attributes;
 
-    alertObject._classes = classes.join(' ');
-    alertObject._attributes = attributes;
+        Adapt.once('notify:closed', function() {
+          Adapt.trigger('tutor:closed', view, alertObject);
+        });
 
-    Adapt.once('notify:closed', function() {
-      Adapt.trigger('tutor:closed', view, alertObject);
-    });
+        Adapt.trigger('notify:popup', alertObject);
 
-    Adapt.trigger('notify:popup', alertObject);
-
-    Adapt.trigger('tutor:opened', view, alertObject);
+        Adapt.trigger('tutor:opened', view, alertObject);
+        break;
+      }
+    }
 
   });
 
